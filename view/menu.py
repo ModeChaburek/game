725925b15f3cded1aceb0966ad7d6ee8965a1712
Timoha <ctexr4130@gmail.com>
2026-02-03 22:52:06 +0700

    update menu

--- view/menu.py
+++ view/menu.py
@@ -9,10 +9,10 @@ class MenuView(arcade.View):
     def on_draw(self):
         self.clear()
         arcade.draw_text(
-            "БАСКЕТ",
+            "Basket Random",
             SCREEN_WIDTH // 2,
             SCREEN_HEIGHT // 2,
-            arcade.color.WHITE,
+            arcade.color.PINK,
             font_size=64,
             anchor_x="center",
             anchor_y="center"
