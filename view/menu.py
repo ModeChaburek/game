e527c90e38554282a5661fb3b0f2f47ff5065d43
Timoha <ctexr4130@gmail.com>
2026-02-04 22:06:19 +0700

    add start, setting, menu

--- view/menu.py
+++ view/menu.py
@@ -1,19 +1,72 @@
 import arcade
+from arcade.gui import *
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 class MenuView(arcade.View):
-    def on_show(self):
-        arcade.set_background_color(arcade.color.WHITE)
+
+    def __init__(self):
+        super().__init__()  # ← ОБЯЗАТЕЛЬНО
+
+        arcade.set_background_color(arcade.color.GRAY)
+
+        self.manager = UIManager()
+        self.manager.enable()
+
+        self.anchor_layout = UIAnchorLayout()
+        self.box_layout = UIBoxLayout(vertical=True, space_between=10)
+
+        self.setup_widgets()
+
+        self.anchor_layout.add(self.box_layout)
+        self.manager.add(self.anchor_layout)
+
+    def setup_widgets(self):
+        play_button = UIFlatButton(
+            text="Играть",
+            width=200
+        )
+
+        setting_button = UIFlatButton(
+            text="Настройки",
+            width=200
+        )
+
+        exit_button = UIFlatButton(
+            text="Выйти",
+            width=200
+        )
+
+        @play_button.event("on_click")
+        def on_play_click(event):
+            print("Играть нажато")
+
+        @setting_button.event("on_click")
+        def on_settings_click(event):
+            print("Настройки")
+
+        @exit_button.event("on_click")
+        def on_exit_click(event):
+            print("Пока пока")
+
+        self.box_layout.add(play_button)
+        self.box_layout.add(setting_button)
+        self.box_layout.add(exit_button)
 
     def on_draw(self):
         self.clear()
+
         arcade.draw_text(
             "Basket Random",
             SCREEN_WIDTH // 2,
-            SCREEN_HEIGHT // 2,
+            SCREEN_HEIGHT // 2 + 200,
             arcade.color.PINK,
             font_size=64,
             anchor_x="center",
             anchor_y="center"
         )
+
+        self.manager.draw()
+
+    def on_hide_view(self):
+        self.manager.disable()
